6085c7ac26ef7c07d053b5406c20c2378f513376
Ahmed Fathy <fathy.dev@gmail.com>
2025-05-19 13:37:53 +0300

    fix issue in form open

--- src/Traits/HasOpenAndClose.php
+++ src/Traits/HasOpenAndClose.php
@@ -2,7 +2,6 @@
 
 namespace Laraeast\LaravelBootstrapForms\Traits;
 
-use Form;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\HtmlString;
 use Illuminate\Support\Str;
@@ -12,66 +11,66 @@ trait HasOpenAndClose
     /**
      * Open the form tag.
      */
-    public function open(string $url, array $attributes = []): \Spatie\Html\Elements\Form
+    public function open(string $url, array $attributes = []): HtmlString
     {
         return html()->form(
             Str::upper(data_get($attributes, 'method', 'GET')),
             $url,
-        )->attributes($attributes);
+        )->attributes($attributes)->open();
     }
 
     /**
      * Open the form tag with get method.
      */
-    public function get(string $url, array $attributes = []): \Spatie\Html\Elements\Form
+    public function get(string $url, array $attributes = []): HtmlString
     {
-        return html()->form(method: 'GET', action: $url)->attributes(attributes: $attributes);
+        return html()->form(method: 'GET', action: $url)->attributes(attributes: $attributes)->open();
     }
 
     /**
      * Open the form tag with post method.
      */
-    public function post(string $url, array $attributes = []): \Spatie\Html\Elements\Form
+    public function post(string $url, array $attributes = []): HtmlString
     {
-        return html()->form(method: 'POST', action: $url)->attributes(attributes: $attributes);
+        return html()->form(method: 'POST', action: $url)->attributes(attributes: $attributes)->open();
     }
 
     /**
      * Open the form tag with put method.
      */
-    public function put(string $url, array $attributes = []): \Spatie\Html\Elements\Form
+    public function put(string $url, array $attributes = []): HtmlString
     {
-        return html()->form(method: 'PUT', action: $url)->attributes(attributes: $attributes);
+        return html()->form(method: 'PUT', action: $url)->attributes(attributes: $attributes)->open();
     }
 
     /**
      * Open the form tag with patch method.
      */
-    public function patch(string $url, array $attributes = []): \Spatie\Html\Elements\Form
+    public function patch(string $url, array $attributes = []): HtmlString
     {
-        return html()->form(method: 'PATCH', action: $url)->attributes(attributes: $attributes);
+        return html()->form(method: 'PATCH', action: $url)->attributes(attributes: $attributes)->open();
     }
 
     /**
      * Open the form tag with delete method.
      */
-    public function delete(string $url, array $attributes = []): \Spatie\Html\Elements\Form
+    public function delete(string $url, array $attributes = []): HtmlString
     {
-        return html()->form(method: 'DELETE', action: $url)->attributes(attributes: $attributes);
+        return html()->form(method: 'DELETE', action: $url)->attributes(attributes: $attributes)->open();
     }
 
     /**
      * Open the form tag with model and put method.
      */
-    public function model(Model $model, string $url, string $method = 'PUT', array $attributes = []): \Spatie\Html\Elements\Form
+    public function model(Model $model, string $url, string $method = 'PUT', array $attributes = []): HtmlString
     {
-        return html()->modelForm(model: $model, method: $method, action: $url)->attributes(attributes: $attributes);
+        return html()->modelForm(model: $model, method: $method, action: $url)->attributes(attributes: $attributes)->open();
     }
 
     /**
      * Open the form tag with model and put method.
      */
-    public function putModel(Model $model, string $url, array $attributes = []): \Spatie\Html\Elements\Form
+    public function putModel(Model $model, string $url, array $attributes = []): HtmlString
     {
         return $this->model($model, $url, 'PUT', $attributes);
     }
@@ -79,7 +78,7 @@ trait HasOpenAndClose
     /**
      * Open the form tag with model and patch method.
      */
-    public function patchModel(Model $model, string $url, array $attributes = []): \Spatie\Html\Elements\Form
+    public function patchModel(Model $model, string $url, array $attributes = []): HtmlString
     {
         return $this->model($model, $url, 'PATCH', $attributes);
     }
