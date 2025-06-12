8e37f665561c6501e7165e436bab0f50c02bca98
Ahmed Fathy <fathy.dev@gmail.com>
2025-06-12 11:52:26 +0300

    fix phone validation rule

--- src/Rules/PhoneNumber.php
+++ src/Rules/PhoneNumber.php
@@ -11,32 +11,56 @@ use Propaganistas\LaravelPhone\Rules\Phone;
 
 class PhoneNumber implements ValidationRule
 {
-    /**
-     * Run the validation rule.
-     *
-     * @param  \Closure(string, ?string=): \Illuminate\Translation\PotentiallyTranslatedString  $fail
-     */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        $supportedCountries = config('laravel-bootstrap-forms.phone.countries', []);
+        $supportedCountries = $this->getSupportedCountryCodes();
+        $countryInput = request("{$attribute}_country");
 
-        $supportedCountries = array_map(fn (Country $country) => $country->getCode(), $supportedCountries);
-
-        if (Validator::make([
-            "{$attribute}_country" => request("{$attribute}_country"),
-        ], [
-            "{$attribute}_country" => [Rule::enum(Country::class), Rule::in($supportedCountries)],
-        ])->fails()) {
-            $fail(__('BsForm::validation.country_not_configured'));
+        if ($countryInput) {
+            if (!$this->isValidCountry($attribute, $countryInput, $supportedCountries)) {
+                $fail(__('BsForm::validation.country_not_configured'));
+                return;
+            }
+            if (!$this->isValidPhone($attribute, $value, $countryInput)) {
+                $fail(__('BsForm::validation.invalid'));
+                return;
+            }
+            return;
         }
 
-        if (Validator::make([
-            $attribute => $value,
-        ], [
-            $attribute => [(new Phone($value))->country(request("{$attribute}_country"))],
-        ]
-        )->fails()) {
+        if (!$this->isValidPhone($attribute, $value, $supportedCountries, true)) {
             $fail(__('BsForm::validation.invalid'));
         }
+    }
+
+    private function getSupportedCountryCodes(): array
+    {
+        $countries = config('laravel-bootstrap-forms.phone.countries', []);
+        return array_map(fn (Country $country) => $country->getCode(), $countries);
+    }
+
+    private function isValidCountry(string $attribute, string $countryInput, array $supportedCountries): bool
+    {
+        $validator = Validator::make([
+            "{$attribute}_country" => $countryInput,
+        ], [
+            "{$attribute}_country" => [Rule::enum(Country::class), Rule::in($supportedCountries)],
+        ]);
+        return !$validator->fails();
+    }
+
+    private function isValidPhone(string $attribute, mixed $value, $country, bool $international = false): bool
+    {
+        $rule = new Phone($value);
+        if ($international) {
+            $rule->international();
+        }
+        $rule->country($country);
+        $validator = Validator::make([
+            $attribute => $value,
+        ], [
+            $attribute => [$rule],
+        ]);
+        return !$validator->fails();
     }
 }
